46ec10f2f03a41e6fcb621c664ed5a0ef099524c
Raivals <103427644+Raivals@users.noreply.github.com>
2023-06-21 14:00:05 +0200

    img carcard

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -27,6 +27,39 @@ const CarCard = ({ car }: CarCardProps) => {
         {carRent}
         <span className="self-end text-[14px] font-medium">/day</span>
       </p>
+
+      <div className="relative object-contain w-full h-40 my-3">
+        <Image
+          src="/hero.png"
+          alt="car model"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
+      <div className="relative flex w-full mt-2">
+        <div className="flex justify-between w-full group-hover:invisible text-gray">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image
+              src="/streering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="steering wheel" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
