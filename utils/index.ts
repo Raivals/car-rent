093345ec0946573c20de45b4a13c6a99a8d03a54
Raivals <103427644+Raivals@users.noreply.github.com>
2023-06-28 14:00:32 +0200

    Dynamic ressearch of cars and parameters

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
-import { CarProps } from "@/types"
+import { CarProps, FilterProps } from "@/types"
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50 // Base rental price per day in dollars
@@ -15,13 +15,14 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0)
 }
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters
   const headers = {
     "X-RapidAPI-Key": "58e042e18cmsh99601ab1b419bbdp1ee568jsnaae06e846ec3",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   }
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     { headers: headers },
   )
 
