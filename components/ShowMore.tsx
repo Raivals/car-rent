ef67d0503dad4b4d796cebfbbb6c93a43905ed45
Raivals <103427644+Raivals@users.noreply.github.com>
2023-06-29 13:16:54 +0200

    Update page tsx miswriting in ShowMore comp

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -2,12 +2,18 @@
 import { ShowMoreProps } from "@/types"
 import { useRouter } from "next/navigation"
 import CustomButton from "./CustomButton"
+import { updateSearchParams } from "@/utils"
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter()
 
   /* logic to handle navigation (be able to show more cars) */
-  const handleNavigation = () => {}
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10
+    const newPathName = updateSearchParams("limit", `${newLimit}`)
+
+    router.push(newPathName)
+  }
 
   return (
     <div className="w-full gap-5 mt-10 flex-center">
